a31f38ed161f8ed972ab5eba2d92e639f52f88ae
teju <tejadeepika1304@gmail.com>
2025-09-10 18:06:03 +0530

    new changes

--- dags/test.py
+++ dags/test.py
@@ -2,27 +2,42 @@ from __future__ import annotations
 
 import pendulum
 
-from airflow.models.dag import DAG
-from airflow.operators.bash import BashOperator
+from airflow.decorators import dag, task
+from datetime import datetime
 
-with DAG(
+# with DAG(
+#     dag_id="basic_test_dag",
+#     start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
+#     schedule=None,
+#     catchup=False,
+#     tags=["basic_tests"],
+# ) as dag:
+#     # Task 1: A simple BashOperator to print a message
+#     task1 = BashOperator(
+#         task_id="print_hello",
+#         bash_command="echo 'Hello from Task 1!'",
+#     )
+#
+#     # Task 2: A simple BashOperator to print a different message
+#     task2 = BashOperator(
+#         task_id="print_world",
+#         bash_command="echo 'Hello from Task 2!'",
+#     )
+#
+#     # Set the dependency: Task 1 must run before Task 2
+#     task1 >> task2
+
+@dag(
     dag_id="basic_test_dag",
-    start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
-    schedule=None,
+    start_date=datetime(2023, 1, 1),
+    schedule='@daily',
     catchup=False,
-    tags=["basic_tests"],
-) as dag:
-    # Task 1: A simple BashOperator to print a message
-    task1 = BashOperator(
-        task_id="print_hello",
-        bash_command="echo 'Hello from Task 1!'",
-    )
+    tags=['basic_tests']
+)
+def basic_tests():
+    @task
+    def task_a():
+        print("hello from Teja here..")
 
-    # Task 2: A simple BashOperator to print a different message
-    task2 = BashOperator(
-        task_id="print_world",
-        bash_command="echo 'Hello from Task 2!'",
-    )
-
-    # Set the dependency: Task 1 must run before Task 2
-    task1 >> task2
+    task()
+basic_tests()
