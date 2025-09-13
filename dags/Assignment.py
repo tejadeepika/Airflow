99c9c9a319f0d9b9c205cbb599d8d2ea87997eaa
teju <tejadeepika1304@gmail.com>
2025-09-13 16:29:53 +0530

    time changes

---
+++ dags/Assignment.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import time
+
+from airflow.decorators import dag, task
+from datetime import datetime
+from airflow.operators.python import PythonOperator
+
+@dag(
+    dag_id= 'random_number_check',
+    start_date=datetime(2025, 9, 13, 10),
+    schedule='@daily',
+    catchup=False,
+    tags=['taskflow']
+)
+def random_number_check():
+    @task
+    def random_number():
+        number=random.randint(1, 100)
+        print('Generated random number: {number}')
+        return number
+    @task
+    def check_odd():
+        result ='even' if number % 2 == 0 else 'odd'
+        print('The number {number} is {result}')
+
+    check_odd(random_number)
+random_number_check()
+
